144474d4c0c50f73e54068e09b7cf5e18f67f690
Guptasparsh321 <Guptasparsh321@gmail.com>
2024-02-07 11:04:21 +0530

    practiced list methods

--- data types/list.py
+++ data types/list.py
@@ -1,3 +1,14 @@
-mylist = ["apple", "banana", "cherry"]
+mylist = ["apple", "banana", "watermelon","cherry"]
 print(mylist[1])
+
+#printing the length of lists
 print(len(mylist))
+
+#sorting the list
+mylist.sort()
+print(mylist[2])
+
+#using the append method
+mylist.append("mango")
+print(mylist)
+
