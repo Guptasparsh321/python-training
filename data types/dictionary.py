dd1ea525b7555348cbc0b326e011f4a7207a05e8
Guptasparsh321 <Guptasparsh321@gmail.com>
2024-02-07 14:48:21 +0530

    practised dictionary

---
+++ data types/dictionary.py
@@ -0,0 +1,16 @@
+#initializing and printing the dictionary
+thisdict = {
+  "name": "sparsh",
+  "age": 22
+}
+print(thisdict)
+
+# modifying the contents of the dictionary
+# thisdict["name"]="gupta"
+# print(thisdict)
+# print(len(thisdict))
+
+# we can access the content of dictionary using get method also.
+print(thisdict.get("name"))
+
+#the various methods that can be are get,pop,popitem etc.
