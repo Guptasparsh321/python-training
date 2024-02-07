bada6484ba2e19b94904bb7ad5d64325fa403884
Guptasparsh321 <Guptasparsh321@gmail.com>
2024-02-07 15:13:11 +0530

    practiced loops and condtionals

---
+++ condtionals and loops/conditionals_and_loops.py
@@ -0,0 +1,47 @@
+# age=int(input())
+# if age>=18:
+#     print("you are eligible to vote")
+# else:
+#     print("you are not eligible to vote")
+    
+
+#while loops
+
+# i=1
+# while i<6:
+#     print(i)
+#     i+=1
+
+# while loop with break statement 
+
+# i=1
+# while i<10:
+#     print(i)
+#     if i>7:
+#         break
+#     i+=1
+
+#while statement with continue.
+
+# i=0
+# while i<5:
+#     i+=1
+#     if i==2:
+#         continue
+#     print(i)
+
+#for loop practice 
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
+
+# for x in range(6):
+#   print(x)
+
+# for x in range(2, 6):
+#   print(x)
+
+for x in range(2, 30, 3):
+  print(x)
+
